1a8ce41378a7c9059d3fd9d79d4bebae92a32f37
UltraAgent18 <thecolewebb@gmail.com>
2023-11-28 15:06:02 -0800

    Player Movement Complete

--- HorrorWalkSim_Group27/Assets/Scripts/PlayerController.cs
+++ HorrorWalkSim_Group27/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
 
+    public float speed = 10f;
+
+    void Update()
+    {
+        //Side to side movement
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+    }
 
 
 
