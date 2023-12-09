fea509ee7f33b83fea1c4b1fe45002462341c08f
UltraAgent18 <thecolewebb@gmail.com>
2023-12-08 17:41:51 -0800

    Started work on jumpscares

---
+++ HorrorWalkSim_Group27/Assets/Scripts/Jumpscare.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumpscare : MonoBehaviour
+{
+    public GameObject JumpScareImg;
+    public AudioSource audioSource;
+
+    private bool isPlayed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        JumpScareImg.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            JumpScareImg.SetActive(true);
+            audioSource.Play();
+            StartCoroutine(DisableImg());
+        }
+    }
+
+    IEnumerator DisableImg()
+    {
+        yield return new WaitForSeconds(2);
+        JumpScareImg.SetActive(false)
+    }
+}
